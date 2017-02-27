44c2aad9221aab9cf88baa67837a9463727dc854
William Hall <willhall3@icloud.com>
2017-02-27 11:47:04 -0500

    Buttons appear as emptyStars and when pressed change to highlightedStars.  The stars do not stay filled after they are selected.

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
 
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     var rating = 0
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet{
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet{
+            setupButtons()
+        }
+    }
     
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -33,16 +43,35 @@ class RatingControl: UIStackView {
     
     //MARK: Private Mehtods
     private func setupButtons(){
+        
+        //clear any existing buttons
+        for button in ratingButtons{
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        //load star images
+        let bundle = Bundle(for: type(of: self))
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
         //loop for 5 buttons
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             //create button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            
+            //set button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             //add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             //setup button acton
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
